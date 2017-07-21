d60d4f2f253920d9e6dcfc922e886df8e2eea58a
nwcrazysword <nwcrazysword@gmail.com>
2017-07-21 16:47:22 +0800

    del the author's info

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,3 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # @Date    : 2017-03-23 17:08:01
-# @Author  : nwcrazysword (nwcrazysword@gmail.com)
-# @Link    : https://github.com/nwcrazysword
-# @Version : $Id$
